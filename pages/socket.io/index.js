eee9e10768e271207e03df4d0266ebc1b460b3de
Unfixingdock <71945647+Roger-ODonnell@users.noreply.github.com>
2023-03-28 14:05:00 +0100

    Project Finished

---
+++ pages/socket.io/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/chat.html");
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
+
+io.on("connection", (socket) => {
+  io.emit("chat message", "User Connected");//User connected chat message
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+  socket.on("disconnect", () => {
+    io.emit("chat message", "User disconnected");//User disconnected chat message
+  });
+});
---
+++ readme.txt
@@ -0,0 +1,7 @@
+Portfolio Tasks:
+
+- Task 3 can be found in the contact page navigated to by scrolling the index page to the enquire button, and clicking enquire.
+- Task 4,6,8,9 and 10 can be found in the projects page navigated to through the navigation bar.
+- Task 7 can be found on the enquire button for transitions and animation can be found in he projects page on the geoLocation api button.
+
+Every page is responsive, styled with css3 and there are more than 5 pages included.
